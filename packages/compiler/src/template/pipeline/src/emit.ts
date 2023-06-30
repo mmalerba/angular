a13c33db208d23bae1b4e9802f15a5c5aa416164
Dylan Hunn <dylhunn@gmail.com>
2023-06-29 17:03:17 -0700

    refactor(compiler): Fix `$event` in listeners for template pipeline
    Previously, `$event` was interpreted as a lexical read on the enclosing context. Now, a new pass converts such reads into simple output AST reads of `$event`, so they are not processed by the context resolution or naming phases. Additionally, the same pass sets a field on the enclosing listener op, so that the reify phase does not have to search for reads of `$event`.

--- packages/compiler/src/template/pipeline/src/emit.ts
+++ packages/compiler/src/template/pipeline/src/emit.ts
@@ -38,6 +38,7 @@ import {phaseSlotAllocation} from './phases/slot_allocation';
 import {phaseTemporaryVariables} from './phases/temporary_variables';
 import {phaseVarCounting} from './phases/var_counting';
 import {phaseVariableOptimization} from './phases/variable_optimization';
+import {phaseResolveDollarEvent} from './phases/resolve_dollar_event';
 
 /**
  * Run all transformation phases in the correct order against a `ComponentCompilation`. After this
@@ -50,6 +51,7 @@ export function transformTemplate(cpl: ComponentCompilation): void {
   phasePureLiteralStructures(cpl);
   phaseGenerateVariables(cpl);
   phaseSaveRestoreView(cpl);
+  phaseResolveDollarEvent(cpl);
   phaseResolveNames(cpl);
   phaseResolveContexts(cpl);
   phaseLocalRefs(cpl);
